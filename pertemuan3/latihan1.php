398bf434803c50219f0aa66fccd3455d153f46b2
putriayu06 <heryanisagi11@gmail.com>
2021-02-18 12:36:31 +0700

    menyelesaikan video pertemuan 3

---
+++ pertemuan3/latihan1.php
@@ -0,0 +1,62 @@
+<?php
+/*
+Ayu Putri Dwi Annisa
+203040166
+https://github.com/putriayu06/pw2021_203040166
+Pertemuan 3 (10 Februari 2021)
+Mempelajari mengenai struktur kendali
+*/
+?>
+<?php
+// Pengulangan
+// for
+// while 
+// do.. while
+// foreach : pengulangan khusus array
+
+// for( $i = 0; $i < 5; $i++ ) {
+//    echo "Hello World! <br>";
+// }
+
+// $i = 10; 
+// while ( $i < 5 ) {
+//    echo "Hello World! <br>";
+// $i++;
+// }
+
+// $i = 10;
+// do {
+//    echo "Hello World! <br>";
+// $i++;
+// } while( $i < 5 );
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Latihan1</title>
+    <style>
+      .warna-baris {
+        background-color: purple;
+      }
+    </style>
+</head>
+<body>
+    
+<table border="1" cellpadding="10" cellspacing="0">
+  <?php for ( $i = 1; $i <= 5; $i++ ) : ?>
+    <?php if ( $i % 2 == 1 ) : ?>
+      <tr class="warna-baris">
+    <?php else : ?>
+      <tr>
+    <?php endif; ?>
+      <?php for ( $j = 1; $j <= 5; $j++ ) : ?>
+          <td><?= "$i, $j"; ?></td>
+      <?php endfor; ?>
+    </tr>
+  <?php endfor; ?>
+</table>
+
+</body>
+</html>
